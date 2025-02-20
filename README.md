375be9d2dacbf95c5e9ae9a2f55ebde6fdb74228
Cechnya <mamiruliksani@gmail.com>
2025-02-19 21:19:56 -0800

    add praktikum1

--- README.md
+++
@@ -1,8 +0,0 @@
-# Pemograman Web 2
-
-## Repositori Praktikum Pemograman Web 2
-
--Nama : Muhammad Amirul Ikhsani  
--NIM : 0110124150
--Rombel :SI11
-
---
+++ variable_user.php
@@ -0,0 +1,12 @@
+<?php
+//mendefinisikan
+$nama = 'ichan' ;
+$umur = 19;
+$berat = 60.60;
+
+echo 'Nama saya ' . $nama;
+echo '<br/>Umur : ' . $umur . 'Tahun';
+echo '<br/>Berat ' . $berat;
+
+
+?>

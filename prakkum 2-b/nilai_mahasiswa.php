cf2e2ab2c23482c03b726419c82564b4ac1217dd
Cechnya <mamiruliksani@gmail.com>
2025-03-05 18:22:32 -0800

    tugas amirul

---
+++ prakkum 2-b/nilai_mahasiswa.php
@@ -0,0 +1,25 @@
+<?php
+$proses = $_POST['proses'];
+$nama_siswa = $_POST['nama'];
+$mata_kuliah = $_POST['matkul'];
+$nilai_uts = $_POST['nilai_uts'];
+$nilai_uas = $_POST['nilai_uas'];
+$nilai_tugas = $_POST['nilai_tugas'];
+$nilai_akhir = $_POST['nilai_akhir'];
+if ($nilai_akhir > 55) {
+    $status = 'Lulus';
+
+} else {
+    $status = 'Tidak lulus';
+}
+
+
+if (!empty($proses)) { 
+    echo 'Proses : ' .$proses;
+    echo '<br/>Nama : ' . $nama_siswa;
+    echo '<br/>Mata Kuliah : ' . $mata_kuliah;
+    echo '<br/>Nilai UTS : ' . $nilai_uts;
+    echo '<br/>Nilai UAS : ' . $nilai_uas;
+    echo '<br/>Nilai Tugas Praktikum : ' . $nilai_tugas;
+    echo '<br/>Nilai Akhir : ' . $nilai_akhir;
+}
